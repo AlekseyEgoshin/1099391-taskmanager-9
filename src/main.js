e4b9083e076bb06d04d219f7e7f1f5f123071205
Aleksey <hl-hanter1997@yandex.ru>
2019-08-21 13:17:21 +0500

    Исправлен код под критерий Б14

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-const CARD_COUNT = 3;
-
-const render = (container, template, place) => container.insertAdjacentHTML(place, template);
-
 import {createFilter} from './components/filter';
 import {createSearchLine} from './components/search-line';
 import {createMenu} from './components/menu';
@@ -13,16 +9,20 @@ import {createTaskEditCard} from './components/edit-task-card';
 import {createBoard} from './components/board';
 import {createButton} from './components/button';
 
+const CARD_COUNT = 3;
+
 const mainElement = document.querySelector(`.main`);
 const mainHeaderElement = mainElement.querySelector(`.main__control`);
+
+const boardElement = mainElement.querySelector(`.board`);
+const taskListElement = mainElement.querySelector(`.board__tasks`);
+
+const render = (container, template, place) => container.insertAdjacentHTML(place, template);
 
 render(mainHeaderElement, createMenu(), `beforeend`);
 render(mainElement, createSearchLine(), `beforeend`);
 render(mainElement, createFilter(), `beforeend`);
 render(mainElement, createBoard(), `beforeend`);
-
-const boardElement = mainElement.querySelector(`.board`);
-const taskListElement = mainElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingLine(), `afterbegin`);
 render(taskListElement, createTaskEditCard(), `beforeend`);
